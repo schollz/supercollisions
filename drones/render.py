e27954ca97cec0dd2b43f8915103ae3bd4ca4272
Zack Scholl <zack.scholl@gmail.com>
2021-09-03 14:14:32 -0700

    add renderer

--- drones/render.py
+++ drones/render.py
@@ -14,7 +14,18 @@ for _, fname in enumerate(files):
         if not os.path.exists(dirname):
             os.makedirs(dirname)
         with open(os.path.join(dirname,fname),"w") as f2:
-            f2.write("s.waitForBoot {\ns.record(path:'/data/"+fname+".aiff',duration:10);\n")
+            f2.write("s.waitForBoot {\n")
+            f2.write("""
+r = Recorder.new(s);
+// record into a flac file
+r.recHeaderFormat = "flac";
+// default 'float' is incompatible with flac. set to 24bit:
+r.recSampleFormat = "int24";
+// set very obvious prefix for files
+r.filePrefix = """+'"'+fname+'"'+""";
+// start recording:
+r.record;
+""")
             f2.write(data)
             f2.write("Routine{11.wait; s.quit;}.play;}")
         os.system("""docker run -v `pwd`/"""+dirname+""":/data -d sc""")
