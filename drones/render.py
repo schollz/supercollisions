e565bbdab9dc8d6c5f8d06bc2a31a64725d15127
Zack Scholl <zack.scholl@gmail.com>
2021-09-03 13:51:45 -0700

    add renderer

--- drones/render.py
+++ drones/render.py
@@ -5,21 +5,20 @@ import glob
 import time
 
 if not os.path.exists("recordings"):
-	os.makedirs("recordings")
+    os.makedirs("recordings")
 
 files=list(glob.glob("*.scd"))
 for _, fname in enumerate(files):
-	if "template" in fname:
-		continue
-	with open(fname,"r") as f:
-		data=f.read()
-		dirname=fname+".sc"
-		if not os.path.exists(dirname):
-			os.makedirs(dirname)
-		with open(os.path.join(dirname,fname),"w") as f2:
-			f2.write("s.waitForBoot {\ns.record(duration:1800);\n")
-			f2.write(data)
-			f2.write("\n}")
-		os.system("""docker run -v `pwd`/"""+dirname+""":/data -v `pwd`/recordings:/root/.local/share/SuperCollider/Recordings -d sc""")
-		break
-	break
+    if "template" in fname:
+        continue
+    with open(fname,"r") as f:
+        data=f.read()
+        dirname=fname+".sc"
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        with open(os.path.join(dirname,fname),"w") as f2:
+            f2.write("s.waitForBoot {\ns.record(duration:60);\n")
+            f2.write(data)
+            f2.write("Routine{60.wait; s.quit;}.play;}")
+        os.system("""docker run -v `pwd`/"""+dirname+""":/data -v `pwd`/recordings:/root/.local/share/SuperCollider/Recordings -d sc""")
+        time.sleep(60)
