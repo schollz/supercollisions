61c30a0f07a42b37b0342353a03a72ef3ee5910a
Zack Scholl <zack.scholl@gmail.com>
2021-09-03 15:11:59 -0700

    remove template

--- drones/render.py
+++ drones/render.py
@@ -1,9 +1,27 @@
 #!/bin/python3
+#
+# before rendering, need to build the docker image:
+#
+#   git clone https://github.com/schollz/supercollider-radio
+#   cd supercollider-radio
+#   docker build -t sc .
+#
+# then all files can be rendered with 
+#   
+#   python3 render.py
+# 
+# and then converted to wav with
+#
+#   for i in */*.aiff; do ffmpeg -i "$i" "${i%.*}.wav"; done   
+#
 
 import os
 import glob
 import time
 
+song_time=20 #minutes
+
+song_time=song_time*60 #seconds
 files=list(glob.glob("*.scd"))
 for _, fname in enumerate(files):
     if "template" in fname:
@@ -16,9 +34,9 @@ for _, fname in enumerate(files):
         with open(os.path.join(dirname,fname),"w") as f2:
             f2.write("s.waitForBoot {\n")
             f2.write("""
-s.record(duration:60,path:"/data/"""+fname+""".aiff");
+s.record(duration:"""+str(song_time)+""",path:"/data/"""+fname+""".aiff");
 """)
             f2.write(data)
-            f2.write("Routine{61.wait; s.quit;}.play;}")
+            f2.write("Routine{"++str(song_time+1)+".wait; s.quit;}.play;}")
         os.system("""docker run -v `pwd`/"""+dirname+""":/data -d sc""")
-        time.sleep(62)
+        time.sleep(song_time+2)
