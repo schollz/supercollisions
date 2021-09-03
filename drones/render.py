1ed6f37fb21095db4063644f6613b044891cd842
Zack Scholl <zack.scholl@gmail.com>
2021-09-03 13:33:56 -0700

    add renderer

---
+++ drones/render.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import os
+import glob
+import time
+
+files=list(glob.glob("*.scd"))
+for _, fname in enumerate(files):
+	if "template" in fname:
+		continue
+	with open(fname,"r") as f:
+		data=f.read()
+		dirname=fname+".sc"
+		if not os.path.exists(dirname):
+			os.makedirs(dirname)
+		os.system("""docker run -v `pwd`/"""+dirname+""":/data -v `pwd`/recordings:/root/.local/share/SuperCollider/Recordings -d sc""")
+		break
